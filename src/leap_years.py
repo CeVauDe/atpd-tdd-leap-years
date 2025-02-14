e10bc51d9fef056ca88321fa27c58f85e517bb57
christian <c_vetter@outlook.de>
2025-02-14 16:46:50 +0100

    refactor: remove duplication

--- src/leap_years.py
+++ src/leap_years.py
@@ -1,9 +1,5 @@
 def is_leap_year(year: int) -> bool:
-    if year == 1900:
-        return False
-    if year == 1800:
-        return False
-    if year == 2300:
+    if year % 100 == 0:
         return False
     if year % 4 == 0:
         return True
