1b46e7d8b90667e86976bcc6ab7da4c35e16df7c
christian <c_vetter@outlook.de>
2025-02-14 16:42:39 +0100

    feat: Add more typical leap year cases

--- src/leap_years.py
+++ src/leap_years.py
@@ -1,4 +1,8 @@
 def is_leap_year(year: int) -> bool:
     if year == 1996:
         return True
+    if year == 1992:
+        return True
+    if year == 1888:
+        return True
     return False
--- test/leap_years.py
+++ test/leap_years.py
@@ -6,7 +6,15 @@ from src.leap_years import is_leap_year
 class TestShouldLeapYear:
 
     @pytest.mark.parametrize(
-        "year,expectation", [(2001, False), (2003, False), (1993, False), (1996, True)]
+        "year,expectation",
+        [
+            (2001, False),
+            (2003, False),
+            (1993, False),
+            (1996, True),
+            (1992, True),
+            (1888, True),
+        ],
     )
     def test_leap_year(self, year: int, expectation: bool):
         assert is_leap_year(year) == expectation
