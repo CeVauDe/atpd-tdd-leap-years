1ec59af49052518cd6e8979bf272335354d17401
christian <c_vetter@outlook.de>
2025-02-14 16:39:08 +0100

    refactor: Remove duplication from test

--- test/leap_years.py
+++ test/leap_years.py
@@ -1,9 +1,12 @@
+import pytest
+
 from src.leap_years import is_leap_year
 
 
 class TestShouldLeapYear:
 
-    def test_leap_year(self):
-        assert is_leap_year(2001) == False
-        assert is_leap_year(2003) == False
-        assert is_leap_year(1993) == False
+    @pytest.mark.parametrize(
+        "year,expectation", [(2001, False), (2003, False), (1993, False)]
+    )
+    def test_leap_year(self, year: int, expectation: bool):
+        assert is_leap_year(year) == expectation
